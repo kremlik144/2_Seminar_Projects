37e976f99fed9eccb900026d5980848553939e71
«Sergey» <fomin-sm1998@mail.ru>
2022-08-25 18:28:20 +0300

    Задачи семинара №9

--- Theme_2/Seminar_9/Program.cs
+++ Theme_2/Seminar_9/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 63 Задайте значение N. Напишите программу которая выведет все натуральные числа в промежутке от 1 до N*/
+
+/*string NumberPrint(int b)
+{
+    if(1 <= b) return NumberPrint(b-1) + $"{b} ";
+    else return string.Empty;
+}
+
+Console.Write("Введите чило N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(NumberPrint(number));*/
+
+
+
+
+
+
+
+
+/*Задача 65 Задайте значение M и N. Напишите программу которая выведет все числа в промежутке от M до N включительно*/
+
+/*string NumberPrint(int a, int b)
+{
+    if(a <= b) return NumberPrint(a, b-1) + $"{b} ";
+    else return string.Empty;
+}
+
+Console.Write("Введите чило M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите чило N: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(NumberPrint(number1, number2));*/
+
+
+
+
+
+
+
+/*Задача 67 Напишите программу которая будет принимать на вход число Х и возвращать сумму его цифр*/
+
+/*int SummDigital(int x)
+{
+    if (x == 0 ) return 0;
+    else return x%10 + SummDigital(x/10);
+}
+
+
+Console.Write("Введите чило N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(SummDigital(number));*/
+
+
+
+
+
+
+
+/*Задача 69 Напишите программу которая будет принимать на вход числа А и Х и возводит число А в степень Х*/
+
+/*int Creata(int n, int x)
+{
+    if (x == 0 ) return 1;
+    else return Creata(n, x-1) * n;
+}
+
+
+Console.Write("Введите чило N: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило Х: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Creata(number1, number2));*/
+
